e5e61844aca8ea6ff51d2e9357b683ff49603475
ken347 <dp27530@gmail.com>
2021-04-04 02:22:51 +0800

    20210404Vue.js學習紀錄

---
+++ day16/src/components/InputComponent/index.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+    name:'inputComponent',
+    props:{
+        text:{
+            type:String,
+            required:true,
+            default:"這是預設"
+        }
+    },
+    computed:{
+        inputText:{
+            get(){
+                return this.text
+            },
+            set(val){
+                //this.$emit('textChange',val) 若只是改值用這種寫法太冗餘
+                this.$emit("update:text",val)
+            }
+        }
+    }
+}
+</script>
+
+<template src="./template.html">
+    
+</template>
+
+<style scoped src="./style.css">
+
+</style>
---
+++ day16/src/components/TitleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+    name:'titleComponent',
+    // data(){
+    //     return {
+    //         titleComponent:"Hello Ken"
+    //     }
+    // }
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color:orange
+}
+</style>
